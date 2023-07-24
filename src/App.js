657b576b27d6c2820327d6d0dfaf4ab256614c81
Subrath sapkota <subrathsapkota119@gmail.com>
2023-07-24 14:03:47 +0545

    Changes in icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./Assets/Profile.jpg";
+import { MdDashboard, MdGroup } from "react-icons/md";
+import { IoMdSchool, IoMdSettings, IoMdExit } from "react-icons/io";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <div className="containerRight">
+          <div className="logoDesignMain">
+            <div className="logodesign"></div>
+            <div className="logo">boosto</div>
+          </div>
+          <div className="profile">
+            <img className="logoImage" src={Logo} alt="" />
+            <span className="profileName">Subrath sapktoa</span>
+            <span className="profileTitle">CEO</span>
+          </div>
+          <div className="items">
+            <ul className="listItems">
+              <li className="listItem">
+                <MdDashboard className="icons" />
+                <span>Dashboard</span>
+              </li>
+              <li className="listItem">
+                <MdGroup className="icons" />
+                <span>Customers</span>
+              </li>
+              <li className="listItem">
+                <IoMdSchool className="icons" />
+                <span>Solutions</span>
+              </li>
+              <li className="listItem">
+                <IoMdSettings className="icons" />
+                <span>Settings</span>
+              </li>
+            </ul>
+          </div>
+          <div className="exit">
+            <IoMdExit className="icons" />
+            Exit
+          </div>
+        </div>
+        <div className="containerLeft">
+          <div className="containerLeftTop">
+            <div className="containerLeftTopLeft"></div>
+            <div className="containerLeftTopRight"></div>
+          </div>
+          <div className="containerLeftBottom"></div>
+        </div>
+      </div>
     </div>
   );
 }
